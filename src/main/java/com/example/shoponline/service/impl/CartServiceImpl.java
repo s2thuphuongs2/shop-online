65946e7df7ed1773905605677862ac2e15db4bf0
BookStore <s2thuphuongs2@gmail.com>
2023-11-22 15:39:15 +0700

    fix authentication error

--- src/main/java/com/example/shoponline/service/impl/CartServiceImpl.java
+++ src/main/java/com/example/shoponline/service/impl/CartServiceImpl.java
@@ -5,8 +5,11 @@ import com.example.shoponline.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.BoundListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,18 +19,37 @@ public class CartServiceImpl implements CartService {
 
 	@Override
 	public Boolean addProductToCart(Object object) {
-		String userId = AuthUtil.getUserId().toString();
-		String cartRedisKey = userId.concat("_CART");
-		redisTemplate.boundListOps(cartRedisKey).rightPush(object);
-		return true;
+		// fixme-done: \"org.springframework.security.core.Authentication.getPrincipal()\"
+		// todo: Kiem tra Xac thuc truoc khi them sp vao Gio hang
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null && authentication.isAuthenticated()){
+			String userId = AuthUtil.getUserId().toString();
+			String cartRedisKey = userId.concat("_CART");
+			redisTemplate.boundListOps(cartRedisKey).rightPush(object);
+			return true;
+		} else {
+			// Xu ly khi khong tim thay thong tin xac thuc
+			return false;
+		}
+
+
 	}
 
 	@Override
 	public List<Object> viewProductInCart() {
-		String userId = AuthUtil.getUserId().toString();
-		String cartRedisKey = userId.concat("_CART");
-		BoundListOperations<String, Object> result = redisTemplate.boundListOps(cartRedisKey);
-		return result.range(0, result.size());
+		// fixme-done: \"org.springframework.security.core.Authentication.getPrincipal()\"
+		// todo: Kiem tra Xac thuc truoc khi xem Gio hang
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null && authentication.isAuthenticated()){
+			String userId = AuthUtil.getUserId().toString();
+			String cartRedisKey = userId.concat("_CART");
+			BoundListOperations<String, Object> result = redisTemplate.boundListOps(cartRedisKey);
+			return result.range(0, result.size());
+		} else {
+			// Xử lý khi không tìm thấy thông tin xác thực
+			return Collections.emptyList();
+		}
+
 	}
 
 	@Override
